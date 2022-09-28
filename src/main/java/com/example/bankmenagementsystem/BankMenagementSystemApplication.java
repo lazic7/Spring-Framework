005ed783aa6939a06a87c766c8abd579b89531dd
Lazic <lazicc9@gmail.com>
2022-09-28 13:22:35 +0200

    initial commit

---
+++ src/main/java/com/example/bankmenagementsystem/BankMenagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.bankmenagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankMenagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankMenagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bankmenagementsystem/BankMenagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bankmenagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankMenagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
